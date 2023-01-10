193659b4fa265e3f08e2d6b214609881d5e4da3b
Øyvind Raddum Berg <elacin@gmail.com>
2023-01-10 14:04:06 +0100

    add select interest

--- src/nodes-edges.ts
+++ src/nodes-edges.ts
@@ -1,37 +1,70 @@
-import {Fag, FagId} from "./model";
+import {Fag, FagId, Interesse} from "./model";
 
-const position = { x: 0, y: 0 };
+const position = {x: 0, y: 0};
 const edgeType = 'smoothstep';
 
-const findNodes = () => {
-    const relevantIds: FagId[] = []
-    Fag.forEach(fag => {
-        if (fag["Obligatoriske forkunnskaper"].length > 0) {
-            relevantIds.push(fag.id);
-        }
-        fag["Obligatoriske forkunnskaper"].forEach(id => relevantIds.push(id))
-    })
-    const relevant = Fag.filter(fag => relevantIds.indexOf(fag.id) !== -1)
+export const mkInteresseMapping = () => {
+    const m = new Map<Interesse, readonly FagId[]>()
+    m.set("Java", [FagId("IN1000"), FagId("IN1020")])
+    m.set("Database", [FagId("IN2090"), FagId("IN3020")])
+    m.set("Programmering", [FagId("IN3040"), FagId("IN2010")])
+    return m;
+}
+export const interesseMapping = mkInteresseMapping();
+export const interesser = [...interesseMapping.keys()];
 
-    return relevant.map(fag =>
-        ({id: fag.id, data: {...fag, label: fag.id}, position, label: fag.id})
+export const findRelevant = (interesse: Interesse | null | undefined): readonly Fag[] => {
+    if (interesse) {
+        const relevantIds: Set<FagId> = new Set();
+        const addDownstream = (fagId: FagId): void => {
+            const fag = Fag.find(fag => fag.id == fagId);
+            if (fag) {
+                relevantIds.add(fagId)
+                fag["Obligatoriske forkunnskaper"].forEach(addDownstream)
+            }
+        }
+        const addUpstream = (fagId: FagId): void => {
+            const fag = Fag.find(fag => fag.id == fagId);
+            if (fag) {
+                Fag.filter(f => f["Obligatoriske forkunnskaper"].indexOf(fagId) !== -1)
+                    .forEach(upstream => {
+                        relevantIds.add(upstream.id)
+                        addUpstream(upstream.id);
+                    })
+            }
+        }
+
+        (interesseMapping.get(interesse) || []).forEach(id => {
+            addDownstream(id)
+            addUpstream(id)
+        })
+
+        return Fag.filter(fag => relevantIds.has(fag.id));
+    }
+    return Fag;
+}
+export const findNodes = (interesse: Interesse | null | undefined) => {
+    const relevant: readonly Fag[] = findRelevant(interesse)
+
+    return relevant.map(fag => {
+            const interesser = interesse ? interesseMapping.get(interesse) : [];
+            return ({id: fag.id, data: {fag, label: fag.id, interesser}, position, label: fag.id});
+        }
     )
 }
-export const initialNodes = findNodes();
 
-const findEdges = () => {
-  const edges: any[] = []
-  initialNodes.forEach(node =>
-      node.data["Obligatoriske forkunnskaper"].forEach(avhengighet =>
-      edges.push(({
-        id: `${node.id}-${avhengighet}`,
-        source: node.id,
-        target: avhengighet,
-        type: edgeType,
-        animated: false
-      }))
-      )
-  )
+export const findEdges = (nodes: ReturnType<typeof findNodes>) => {
+    const edges: any[] = []
+    nodes.forEach(node =>
+        node.data.fag["Obligatoriske forkunnskaper"].forEach(avhengighet =>
+            edges.push(({
+                id: `${node.id}-${avhengighet}`,
+                source: node.id,
+                target: avhengighet,
+                type: edgeType,
+                animated: false
+            }))
+        )
+    )
     return edges;
 }
-export const initialEdges = findEdges();
--- src/skilltree.tsx
+++ src/skilltree.tsx
@@ -1,11 +1,12 @@
-import React, { useCallback } from 'react';
-import ReactFlow, { addEdge, ConnectionLineType, useNodesState, useEdgesState, Edge } from 'reactflow';
+import React, {useCallback, useState} from 'react';
+import ReactFlow, {useEdgesState, useNodesState} from 'reactflow';
 import {graphlib, layout} from 'dagre';
 import 'reactflow/dist/style.css';
-
-import { initialNodes, initialEdges } from './nodes-edges';
-
+import {findEdges, findNodes, interesser} from './nodes-edges';
 import './index.css';
+import {Interesse} from "./model";
+import FormControl from '@mui/material/FormControl';
+import {InputLabel, MenuItem, Select} from "@mui/material";
 
 const dagreGraph = new graphlib.Graph();
 dagreGraph.setDefaultEdgeLabel(() => ({}));
@@ -14,76 +15,81 @@ const nodeWidth = 172;
 const nodeHeight = 36;
 
 const getLayoutedElements = (nodes: any[], edges: any[], direction = 'TB') => {
-  const isHorizontal = direction === 'LR';
-  dagreGraph.setGraph({ rankdir: direction });
+    const isHorizontal = direction === 'LR';
+    dagreGraph.setGraph({rankdir: direction});
 
-  nodes.forEach((node) => {
-    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
-  });
+    nodes.forEach((node) => {
+        dagreGraph.setNode(node.id, {width: nodeWidth, height: nodeHeight});
+    });
 
-  edges.forEach((edge) => {
-    dagreGraph.setEdge(edge.source, edge.target);
-  });
+    edges.forEach((edge) => {
+        dagreGraph.setEdge(edge.source, edge.target);
+    });
 
-  layout(dagreGraph);
+    layout(dagreGraph);
 
-  nodes.forEach((node) => {
-    const nodeWithPosition = dagreGraph.node(node.id);
-    node.targetPosition = isHorizontal ? 'left' : 'top';
-    node.sourcePosition = isHorizontal ? 'right' : 'bottom';
+    nodes.forEach((node) => {
+        const nodeWithPosition = dagreGraph.node(node.id);
+        node.targetPosition = isHorizontal ? 'left' : 'top';
+        node.sourcePosition = isHorizontal ? 'right' : 'bottom';
 
-    // We are shifting the dagre node position (anchor=center center) to the top left
-    // so it matches the React Flow node anchor point (top left).
-    node.position = {
-      x: nodeWithPosition.x - nodeWidth / 2,
-      y: nodeWithPosition.y - nodeHeight / 2,
-    };
+        // We are shifting the dagre node position (anchor=center center) to the top left
+        // so it matches the React Flow node anchor point (top left).
+        node.position = {
+            x: nodeWithPosition.x - nodeWidth / 2,
+            y: nodeWithPosition.y - nodeHeight / 2,
+        };
 
-    return node;
-  });
+        return node;
+    });
 
-  return { nodes, edges };
+    return {nodes, edges};
 };
 
-const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
-  initialNodes,
-  initialEdges
-);
-
 const SkillTree = () => {
-  const [nodes, setNodes, onNodesChange] = useNodesState(layoutedNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(layoutedEdges);
+    const [interesse, setInteresse] = useState<Interesse | undefined>(undefined)
+    const layouted = getLayoutedElements([], []);
+    const [nodes, setNodes, onNodesChange] = useNodesState(layouted.nodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(layouted.edges);
 
-  const onLayout = useCallback(
-    (direction: string) => {
-      const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
-        nodes,
-        edges,
-        direction
-      );
+    const onLayout = useCallback(
+        (interesse: Interesse) => {
+            setInteresse(interesse)
+            const initialNodes = findNodes(interesse);
+            const initialEdges = findEdges(initialNodes);
+            const layouted = getLayoutedElements(initialNodes, initialEdges);
+            setNodes([...layouted.nodes]);
+            setEdges([...layouted.edges]);
+        },
+        [nodes, edges, interesse]
+    );
+    console.warn(interesser)
 
-      setNodes([...layoutedNodes]);
-      setEdges([...layoutedEdges]);
-    },
-    [nodes, edges]
-  );
+    const renderedInteresser = interesser.map((i, idx) => <MenuItem key={idx} value={i}>{i}</MenuItem>)
 
-  return (
-    <div className="layoutflow">
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        nodesDraggable={false}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        fitView
-      />
-      <div className="controls">
-        <button onClick={() => onLayout('TB')}>vertical layout</button>
-        <button onClick={() => onLayout('LR')}>horizontal layout</button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="layoutflow">
+            <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Velg interesse</InputLabel>
+                <Select
+                    value={interesse || ""}
+                    label="Velg interesse"
+                    onChange={(ev) => {
+                        onLayout(ev.target.value)
+                    }}
+                > {renderedInteresser}
+                </Select>
+            </FormControl>
+            <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                nodesDraggable={false}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                fitView
+            />
+        </div>
+    );
 };
 
 export default SkillTree;
